99b5735f961ed92e62de5aec2aa415ed5c3ce5f8
league <league@wts13.attlocal.net>
2019-03-27 18:32:43 -0700

    BACKPACK ip

--- src/backpack/BackpackRunner.java
+++ src/backpack/BackpackRunner.java
@@ -3,5 +3,11 @@ package backpack;
 public class BackpackRunner {
 	public static void main(String[] args) {
 		new Backpack().packAndCheck();
+		Pencil x = new Pencil();
+		Ruler y = new Ruler();
+		
+		new Backpack().putInBackpack(x);
+		new Backpack().putInBackpack(y);
+		new Backpack().putinBackpack(z);
 	}
 }
