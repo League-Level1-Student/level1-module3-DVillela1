6defafae3e109b7d83eb8b3cd23bef18a273cefa
league <league@wts22.attlocal.net>
2019-04-10 18:25:29 -0700

    JukeBox, MagicBox ip

--- src/jukebox/Jukebox.java
+++ src/jukebox/Jukebox.java
@@ -4,7 +4,6 @@ package jukebox;
  *    Level 1
  */
 
-
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.FileInputStream;
@@ -18,7 +17,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 
 import javazoom.jl.player.advanced.AdvancedPlayer;
 
@@ -26,11 +24,12 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
  *   Right click your project and add it as a JAR (Under Java Build Path > Libraries).*/
 
 public class Jukebox implements Runnable, MouseListener {
-	
-		static JButton unbothered = new JButton();		
-		static JButton makebelieve = new JButton();		
-		static JButton anything = new JButton();
-    public void run() {
+
+	static JButton unbothered = new JButton();
+	static JButton makebelieve = new JButton();
+	static JButton anything = new JButton();
+
+	public void run() {
 
 		// 1. Find an mp3 on your computer or on the Internet.
 		// 2. Create a Song object for that mp3
@@ -38,11 +37,10 @@ public class Jukebox implements Runnable, MouseListener {
 		// 3. Play the Song
 
 		/*
-		 * 4. Create a user interface for your Jukebox so that the user can to
-		 * choose which song to play. You can use can use a different button for
-		 * each song, or a picture of the album cover. When the button or album
-		 * cover is clicked, stop the currently playing song, and play the one
-		 * that was selected.
+		 * 4. Create a user interface for your Jukebox so that the user can to choose
+		 * which song to play. You can use can use a different button for each song, or
+		 * a picture of the album cover. When the button or album cover is clicked, stop
+		 * the currently playing song, and play the one that was selected.
 		 */
 		JFrame frame = new JFrame();
 		frame.setTitle("Play Song");
@@ -64,9 +62,8 @@ public class Jukebox implements Runnable, MouseListener {
 		panel.add(makebelieve);
 		makebelieve.setVisible(true);
 		frame.pack();
-    }
-    
-    
+	}
+
 	/* Use this method to add album covers to your Panel. */
 	private JLabel loadImage(String fileName) {
 		URL imageURL = getClass().getResource(fileName);
@@ -74,41 +71,47 @@ public class Jukebox implements Runnable, MouseListener {
 		return new JLabel(icon);
 	}
 
-
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		JButton pressed = (JButton) e.getSource();
-		if (pressed = this.anything) {
-			new Song("Anything.mp3");
+		if (pressed == anything) {
+			new Song("Anything.mp3").play();
+		}
+		if (pressed == unbothered) {
+			new Song("Unbothered.mp3").play();
+		}
+		if (pressed == makebelieve) {
+			new Song("Make Believe.mp3").play();
 		}
 	}
 
-
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
-
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
-
+	// TODO Auto-generated method stub
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		JButton pressed = (JButton) e.getSource();
+		if (pressed == anything) {
+			new Song("Anything.mp3").stop();
+		}
+		if (pressed == unbothered) {
+			new Song("Unbothered.mp3").stop();
+		}
+		if (pressed == makebelieve) {
+			new Song("Make Believe.mp3").stop();
+		}
 	}
 
 }
@@ -121,8 +124,7 @@ class Song {
 	private InputStream songStream;
 
 	/**
-	 * Songs can be constructed from files on your computer or Internet
-	 * addresses.
+	 * Songs can be constructed from files on your computer or Internet addresses.
 	 * 
 	 * Examples: <code> 
 	 * 		new Song("everywhere.mp3"); 	//from default package 
@@ -197,4 +199,3 @@ class Song {
 		}
 	}
 }
-
