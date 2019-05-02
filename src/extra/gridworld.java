f261cbb8bf9f86ef76bd8d7c64a5ced470bfc80b
league <league@wts22.attlocal.net>
2019-05-01 18:23:35 -0700

    gridworld

--- src/extra/gridworld.java
+++ src/extra/gridworld.java
@@ -1,5 +1,41 @@
 package extra;
 
+import java.awt.Color;
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Flower;
+import info.gridworld.grid.Location;
+import info.gridworld.world.World;
+
 public class gridworld {
 
+	private World world;
+	private Location loc;
+	private Flower flower;
+
+	public World() {
+		this.world = world;
+	}
+
+	public void Show() {
+		world.show();
+	}
+
+	public void add(Location loc, Actor occupant) {
+		world.add(loc, occupant);
+
+	}
+
+	public void Turn() {
+
+	}
+
+	public Flower() {
+		this.flower = flower;
+		
+	}
+
+	public void setColor(Color newColor) {
+		this.newColor = olor;
+	}
 }
